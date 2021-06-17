e12434db89056d07b8c79954825fe5b2e00fca3f
ahmed taha fekry <35856483+atf01@users.noreply.github.com>
2021-06-17 20:16:54 +0200

    Update Searcher.py

--- Searcher.py
+++ Searcher.py
@@ -5,7 +5,7 @@ import glob
 import os
 
 class Searcher:
-    def __init__(self, indexPath):  # store our index path
+    def __init__(self, indexPath):  # store the index path
         self.results=dict()
         self.indexPath = indexPath
         with open(self.indexPath) as f:
@@ -20,20 +20,22 @@ class Searcher:
             f.close()
 
     def search(self, queryFeatures, limit=10):
-        # initialize our dictionary of results
+        # initialize the dictionary of results
         # open the index file for reading
         # initialize the CSV reader
 
-        # loop over the rows in the index
+        # loop over rows in the index
         for key,value in self.results.items():
             print(key,value)
             d = self.chi2_distance(value, queryFeatures)
 
-            # now that we have the distance between the two feature
-            # vectors, we can udpate the results dictionary -- the
-            # key is the current image ID in the index and the
-            # value is the distance we just computed, representing
-            # how 'similar' the image in the index is to our query
+            '''
+            now that we have the distance between the two feature
+            vectors, we can udpate the results dictionary -- the
+            key is the current image ID in the index and the
+            value is the distance we just computed, representing
+            how 'similar' the image in the index is to our query
+            '''
             self.results[key] = d
 
         # close the reader
